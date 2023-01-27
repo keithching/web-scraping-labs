81eee3a53cce12af74bca14a11ac53f6704a1c09
Keith <keithching91@gmail.com>
2023-01-27 22:43:36 +0900

    feat: get html data and download to file system

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 - Docker
 - python
+
+# issues
+
+- the HTML data got from the URL does not contain all the HTML li elements from the website. Only a portion of the photos is downloaded as a result.
--- script.py
+++ script.py
@@ -1,1 +1,46 @@
-print("hello world!")
+# http request
+import requests
+# scraping
+from bs4 import BeautifulSoup
+import urllib.request
+import os
+
+def getdata(url):
+    r = requests.get(url)
+    return r.text
+
+HOST = "https://sakurazaka46.com"
+
+htmldata = getdata("https://sakurazaka46.com/s/s46/contents_list?ima=2445&cd=104&ct=fc_photo_048&so=ID")
+
+with open('html_data.txt', 'w') as f:
+    f.write(htmldata)
+
+# soup = BeautifulSoup(htmldata, 'html.parser')
+
+# # create a folder in the file system in the container
+# # a bind mount is used to mirror to the local machine
+# def my_makedirs(path):
+#     if not os.path.isdir(path):
+#         os.makedirs(path)
+
+# IMAGE_DIR = 'images'
+
+# my_makedirs(IMAGE_DIR)
+
+# with open('url_list.txt', 'w') as f:
+#     for index, item in enumerate(soup.find_all('img')):
+#         style = item.get('style')
+#         if (style):
+#             url = style.split('(', 1)[1].split(')')[0] # getting the substring within the parenthesis
+#             url = url.replace('/300_300_102400', '') # trim string
+#             fullUrl = HOST + url
+#             f.write(fullUrl)
+#             f.write('\n')
+            
+#             filepath = IMAGE_DIR + '/' + str(index) + '.jpg'
+#             # only write to file system if not exist
+#             if not os.path.exists(filepath):
+#                 urllib.request.urlretrieve(fullUrl, filepath)
+
+# print('operation completed')
